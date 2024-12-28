49b0754339f2466be7e8489ba11fc4a08b52fe30
vivek.z-at-596270005645 <vivek@zinbay.in>
2024-12-28 23:53:13 +0530

    28/12/2

--- naarad/web/views.py
+++ naarad/web/views.py
@@ -5,3 +5,7 @@ def index(request):
     context = {"is_index": True}
     return render(request, "web/index.html", context)
 
+def about(request):
+    context = {"is_index": True}
+    return render(request, "web/about.html", context)
+
